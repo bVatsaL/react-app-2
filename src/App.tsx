c81561141b62d3ceb5a0ba41e489ea18dbd7b324
Vatsal Budhbhatti <vatsal@atyantik.com>
2023-03-17 05:13:48 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BaseSyntheticEvent, useState } from "react";
+import "./App.css";
 
-function App() {
+const API_URL = process.env.API_URL || 'https://app2-5gbx.onrender.com';
+
+const App: React.FC = () => {
+  const [items, setItems] = useState([]);
+
+  const getItems = () => {
+    fetch(`${API_URL}/api`)
+      .then((res) => res.json())
+      .then((result) => setItems(result.data));
+  };
+
+  const clearItems = (e: BaseSyntheticEvent) => {
+    fetch(`${API_URL}/api/clear`)
+      .then((res) => res.json())
+      .then((result) => setItems(result.data));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <div>Items storege</div>
+          <button
+            className="buttonStyle"
+            type="button"
+            onClick={getItems}
+          >
+            Refresh
+          </button>
+          <button
+            className="buttonStyle"
+            type="button"
+            onClick={clearItems}
+          >
+            Clear storage
+          </button>
+        </div>
+        <div>
+          {!items.length && (
+            <span>Empty.!</span>
+          )}
+          <ul>
+            {!!items.length && items.map((item) => (<li key={item}>{item}</li>))}
+          </ul>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
